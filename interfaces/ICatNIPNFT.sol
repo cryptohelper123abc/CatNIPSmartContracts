ca68d27ba216a53714bc083192ea136ee91523d8
Henry Case <87102497+cryptohelper123abc@users.noreply.github.com>
2021-08-16 01:54:09 -0400

    Interfaces

---
+++ interfaces/ICatNIPNFT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.6;
+
+
+interface ICatNIPNFT {
+
+    function RandomNumberForGamesViewable() external view returns (uint256);
+
+    function ADJFKLSDNLKDASGAUJKLSDJFLAK() external view returns (uint256);
+    function ADJFKLSDNLDAKDASGAUJKLSDJFLAK() external view returns (uint256);
+    function ADJFKLSDNLKfdDASGASUJKLSDJFLAK() external view returns (uint256);
+    function ADJFKLSSSDNLKDASGAUJKLSDJFFDSADSALAK() external view returns (uint256);
+    function ADJFKLSDNLKFDAYUDASGAUJKLSDJFLAKK() external view returns (string memory);
+    function JJJADJFKLSDNLKfdDASGASUJKLSDJFLAK() external view returns (address);
+
+
+}
